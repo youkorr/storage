027f4604ed94ce8aef54fce9abde2671db0ce1c1
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-08 22:44:48 +0100

    Update storage.h

--- components/storage/storage.h
+++ components/storage/storage.h
@@ -8,26 +8,25 @@
 namespace esphome {
 namespace storage {
 
+class StorageFile : public Component {
+ public:
+  void set_path(const std::string &path) { path_ = path; }
+  std::string get_path() const { return path_; }
+
+ private:
+  std::string path_;
+};
+
 class StorageComponent : public Component {
  public:
   void set_platform(const std::string &platform) { platform_ = platform; }
   void set_web_server(web_server_base::WebServerBase *web_server) { web_server_ = web_server; }
   
-  void add_file(const std::string &path, const std::string &id) {
-    files_.emplace_back(path, id);
+  void add_file(StorageFile *file) {
+    files_.push_back(file);
   }
   
-  std::string get_file_url(const std::string &id) const {
-    for (const auto &file : files_) {
-      if (file.second == id) {
-        return "/media/" + file.second;
-      }
-    }
-    return "";
-  }
-
-  std::vector<uint8_t> read_file(char const *path);
-  std::vector<uint8_t> read_file(std::string const &path);
+  std::vector<uint8_t> read_file(StorageFile *file);
 
   void setup() override;
   void on_setup_web_server();
@@ -36,12 +35,12 @@ class StorageComponent : public Component {
   void setup_sd_card();
   void setup_flash();
   void setup_inline();
-  void serve_file(const std::string &path, const std::string &id);
+  void serve_file(StorageFile *file);
 
  private:
   std::string platform_;
   web_server_base::WebServerBase *web_server_{nullptr};
-  std::vector<std::pair<std::string, std::string>> files_;
+  std::vector<StorageFile*> files_;
 };
 
 }  // namespace storage
