ee8bc6c6f24e93ec27f336463ea15226588ea1ed
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-08 23:29:37 +0100

    Update storage.h

--- components/storage/storage.h
+++ components/storage/storage.h
@@ -1,7 +1,7 @@
 #pragma once
-
 #include "esphome/core/component.h"
 #include "esphome/components/web_server_base/web_server_base.h"
+#include <ESPAsyncWebServer.h>  // Ajouté pour AsyncWebServerRequest
 #include <vector>
 #include <string>
 
@@ -12,11 +12,15 @@ class StorageFile : public Component {
  public:
   void set_path(const std::string &path) { path_ = path; }
   std::string get_path() const { return path_; }
-
   std::vector<uint8_t> read();
-
+  
+  // Ajout de la plateforme pour le fichier également
+  void set_platform(const std::string &platform) { platform_ = platform; }
+  std::string get_platform() const { return platform_; }
+  
  private:
   std::string path_;
+  std::string platform_;  // Ajouté cette variable manquante
 };
 
 class StorageComponent : public Component {
@@ -30,13 +34,14 @@ class StorageComponent : public Component {
   
   void setup() override;
   void on_setup_web_server();
-
+  
  protected:
   void setup_sd_card();
   void setup_flash();
   void setup_inline();
-  void serve_file(StorageFile *file, web_server_base::Request *req);
-
+  // Modification du type Request vers AsyncWebServerRequest
+  void serve_file(StorageFile *file, AsyncWebServerRequest *req);
+  
  private:
   std::string platform_;
   web_server_base::WebServerBase *web_server_{nullptr};
