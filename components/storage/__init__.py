f11d4d0f6f9da770e6e980e51410b57d2c70bf2c
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-09 00:17:03 +0100

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -6,7 +6,6 @@ from esphome.components import web_server_base, audio
 DEPENDENCIES = ['media_player', 'web_server_base', 'audio']
 CODEOWNERS = ["@votre_nom"]
 
-# Déclaration des IDs
 CONF_STORAGE = "storage"
 CONF_FILES = "files"
 CONF_PATH = "path"
@@ -14,7 +13,7 @@ CONF_MEDIA_FILE = "media_file"
 
 storage_ns = cg.esphome_ns.namespace('storage')
 StorageComponent = storage_ns.class_('StorageComponent', cg.Component)
-StorageFile = storage_ns.class_('StorageFile', audio.AudioFile, cg.Component)
+StorageFile = storage_ns.class_('StorageFile', audio.AudioFile, cg.Component)  # Héritage double
 
 FILE_SCHEMA = cv.Schema({
     cv.Required(CONF_PATH): cv.string,
@@ -32,18 +31,15 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     
-    # Définit la plateforme dans StorageComponent
     cg.add(var.set_platform(config[CONF_PLATFORM]))
     
-    # Propage la plateforme à chaque fichier
     for file in config[CONF_FILES]:
         file_var = cg.new_Pvariable(file[CONF_ID])
         cg.add(file_var.set_path(file[CONF_PATH]))
-        cg.add(file_var.set_platform(config[CONF_PLATFORM]))  # Ligne ajoutée
+        cg.add(file_var.set_platform(config[CONF_PLATFORM]))
+        await cg.register_component(file_var, file)  # Enregistre comme Component
         cg.add(var.add_file(file_var))
-        await cg.register_component(file_var, file)
     
-    # Configuration du serveur web
     if CONF_WEB_SERVER in config:
         web_server = await cg.get_variable(config[CONF_WEB_SERVER])
         cg.add(var.set_web_server(web_server))
