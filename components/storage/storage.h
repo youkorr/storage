02fb380238f717d941d3d9cec3322ebcdaf8b810
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-08 23:37:25 +0100

    Update storage.h

--- components/storage/storage.h
+++ components/storage/storage.h
@@ -1,6 +1,6 @@
 #pragma once
+
 #include "esphome/core/component.h"
-#include "esphome/components/web_server_base/web_server_base.h"
 #include <ESPAsyncWebServer.h>
 #include <vector>
 #include <string>
@@ -12,19 +12,17 @@ class StorageFile : public Component {
  public:
   void set_path(const std::string &path) { path_ = path; }
   std::string get_path() const { return path_; }
-  void set_platform(const std::string &platform) { platform_ = platform; }
-  std::string get_platform() const { return platform_; }
+
   std::vector<uint8_t> read();
-  
+
  private:
   std::string path_;
-  std::string platform_;
 };
 
 class StorageComponent : public Component {
  public:
   void set_platform(const std::string &platform) { platform_ = platform; }
-  void set_web_server(web_server_base::WebServerBase *web_server) { web_server_ = web_server; }
+  void set_web_server(AsyncWebServer *web_server) { web_server_ = web_server; }
   
   void add_file(StorageFile *file) {
     files_.push_back(file);
@@ -32,32 +30,17 @@ class StorageComponent : public Component {
   
   void setup() override;
   void on_setup_web_server();
-  
+
  protected:
   void setup_sd_card();
   void setup_flash();
   void setup_inline();
-  void serve_file(StorageFile *file, AsyncWebServerRequest *request);
-  
+  void serve_file(StorageFile *file, AsyncWebServerRequest *req);
+
  private:
   std::string platform_;
-  web_server_base::WebServerBase *web_server_{nullptr};
+  AsyncWebServer *web_server_{nullptr};
   std::vector<StorageFile*> files_;
-};
-
-// Définition anticipée pour l'utilisation dans storage.cpp
-class AsyncCallbackWebHandler : public AsyncWebHandler {
-public:
-  using RequestCallback = std::function<void(AsyncWebServerRequest*)>;
-  
-  AsyncCallbackWebHandler(const char* uri, RequestCallback callback);
-  
-  bool canHandle(AsyncWebServerRequest *request) override;
-  void handleRequest(AsyncWebServerRequest *request) override;
-
-private:
-  std::string uri_;
-  RequestCallback callback_;
 };
 
 }  // namespace storage
