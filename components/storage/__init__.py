d8e18e96ac576b735ae5a32edff92067ec3088fb
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-08 21:56:53 +0100

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -1,9 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_PLATFORM
-from esphome import automation
+from esphome.const import CONF_ID, CONF_PLATFORM, CONF_WEB_SERVER
+from esphome.components import web_server_base
 
-DEPENDENCIES = ['media_player']
+DEPENDENCIES = ['media_player', 'web_server_base']
 CODEOWNERS = ["@votre_nom"]
 
 CONF_STORAGE = "storage"
@@ -14,17 +14,16 @@ CONF_MEDIA_FILE = "media_file"
 storage_ns = cg.esphome_ns.namespace('storage')
 StorageComponent = storage_ns.class_('StorageComponent', cg.Component)
 
-# Schema for individual file entry
 FILE_SCHEMA = cv.Schema({
     cv.Required(CONF_SOURCE): cv.string,
     cv.Required(CONF_ID): cv.string,
 })
 
-# Main storage schema
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_PLATFORM): cv.one_of("sd_card", "flash", "inline", lower=True),
     cv.Required(CONF_ID): cv.declare_id(StorageComponent),
     cv.Required(CONF_FILES): cv.ensure_list(FILE_SCHEMA),
+    cv.Optional(CONF_WEB_SERVER): cv.use_id(web_server_base.WebServerBase),
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
@@ -33,7 +32,10 @@ def to_code(config):
     
     cg.add(var.set_platform(config[CONF_PLATFORM]))
     
-    # Register each file ID globally
+    if CONF_WEB_SERVER in config:
+        web_server = yield cg.get_variable(config[CONF_WEB_SERVER])
+        cg.add(var.set_web_server(web_server))
+    
     for file in config[CONF_FILES]:
         cg.add(var.add_file(file[CONF_SOURCE], file[CONF_ID]))
         cg.register_variable(file[CONF_ID], var)
