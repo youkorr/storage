5c2dcf3706dcbf5c4586743441514396061ccf19
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-08 23:17:28 +0100

    Update storage.h

--- components/storage/storage.h
+++ components/storage/storage.h
@@ -1,17 +1,20 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/web_server/web_server.h"
+#include "esphome/components/web_server_base/web_server_base.h"
+#include "esphome/components/audio/audio_file.h"
 #include <vector>
 #include <string>
 
 namespace esphome {
 namespace storage {
 
-class StorageFile : public Component {
+class StorageFile : public audio::AudioFile, public Component {
  public:
   void set_path(const std::string &path) { path_ = path; }
   std::string get_path() const { return path_; }
+
+  std::vector<uint8_t> read() override;
 
  private:
   std::string path_;
@@ -26,8 +29,6 @@ class StorageComponent : public Component {
     files_.push_back(file);
   }
   
-  std::vector<uint8_t> read_file(StorageFile *file);
-
   void setup() override;
   void on_setup_web_server();
 
@@ -35,7 +36,7 @@ class StorageComponent : public Component {
   void setup_sd_card();
   void setup_flash();
   void setup_inline();
-  void serve_file(StorageFile *file);
+  void serve_file(StorageFile *file, web_server_base::Request *req);
 
  private:
   std::string platform_;
