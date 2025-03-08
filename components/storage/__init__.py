1c58acbe57d9a0a5b2d09ffe94d164af23399ea2
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-08 22:15:26 +0100

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -8,21 +8,21 @@ CODEOWNERS = ["@votre_nom"]
 
 CONF_STORAGE = "storage"
 CONF_FILES = "files"
-CONF_SOURCE = "source"
+CONF_DATA = "data"
 CONF_MEDIA_FILE = "media_file"
 
 storage_ns = cg.esphome_ns.namespace('storage')
 StorageComponent = storage_ns.class_('StorageComponent', cg.Component)
 
-def validate_source(value):
+def validate_data(value):
   if isinstance(value, str):
     return cv.string(value)
   if isinstance(value, dict) and 'lambda' in value:
-    return cv.returning_lambda(value['lambda'])
-  raise cv.Invalid("Source must be either a string or a lambda")
+    return cv.templatable(value['lambda'])
+  raise cv.Invalid("Data must be either a string or a lambda")
 
 FILE_SCHEMA = cv.Schema({
-    cv.Required(CONF_SOURCE): validate_source,
+    cv.Required(CONF_DATA): validate_data,
     cv.Required(CONF_ID): cv.string,
 })
 
@@ -44,13 +44,13 @@ def to_code(config):
         cg.add(var.set_web_server(web_server))
     
     for file in config[CONF_FILES]:
-        if isinstance(file[CONF_SOURCE], str):
+        if isinstance(file[CONF_DATA], str):
             cg.add(var.add_file(
-                lambda path=file[CONF_SOURCE]: id(var).read_file(path),
+                lambda path=file[CONF_DATA]: id(var).read_file(path),
                 file[CONF_ID]
             ))
         else:
-            cg.add(var.add_file(file[CONF_SOURCE], file[CONF_ID]))
+            cg.add(var.add_file(file[CONF_DATA], file[CONF_ID]))
         cg.register_variable(file[CONF_ID], var)
 
 
