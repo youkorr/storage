c3abfadfdf147e911163f6e9b244f4ce5de47ce8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-09 01:27:51 +0100

    Update storage.h

--- components/storage/storage.h
+++ components/storage/storage.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "esphome/core/component.h"
 #include "esp_http_server.h"
 #include "esp_log.h"
@@ -14,12 +13,19 @@ class StorageComponent;
 
 class StorageFile : public Component {
  public:
+  // Constructeur original qui prend un parent
   StorageFile(StorageComponent *parent) : parent_(parent) {}
+  
+  // Ajout d'un constructeur par défaut
+  StorageFile() : parent_(nullptr) {}
+  
   void set_path(const std::string &path) { path_ = path; }
   std::string get_path() const { return path_; }
-
   std::vector<uint8_t> read();
-
+  
+  // Ajout d'une méthode pour définir le parent après la construction
+  void set_parent(StorageComponent *parent) { parent_ = parent; }
+  
  private:
   std::string path_;
   StorageComponent *parent_;
@@ -31,20 +37,23 @@ class StorageComponent : public Component {
   void set_web_server(httpd_handle_t server) { server_ = server; }
   
   void add_file(StorageFile *file) {
+    // Si le fichier n'a pas de parent, définir this comme parent
+    if (file->parent_ == nullptr) {
+      file->set_parent(this);
+    }
     files_.push_back(file);
   }
   
   void setup() override;
   void on_setup_web_server();
-
   std::string get_platform() const { return platform_; }
-
+  
  protected:
   void setup_sd_card();
   void setup_flash();
   void setup_inline();
   esp_err_t serve_file(httpd_req_t *req);
-
+  
  private:
   std::string platform_;
   httpd_handle_t server_{nullptr};
