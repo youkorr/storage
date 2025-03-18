5796f45467cc06ff2732d6afcb0737839deda81e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-18 18:03:31 +0100

    Update storage.h

--- components/storage/storage.h
+++ components/storage/storage.h
@@ -3,13 +3,7 @@
 #include <vector>
 #include "esphome/core/component.h"
 #include "esphome/components/audio/audio.h"
-
-// Forward declaration of SDMMCCard class
-namespace esphome {
-namespace sd_mmc_card {
-class SDMMCCard;
-}  // namespace sd_mmc_card
-}  // namespace esphome
+#include "esphome/components/sd_mmc_card/sd_mmc_card.h"
 
 namespace esphome {
 namespace storage {
