91833558ca6150f0d89c86f77d096e7008961f85
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-18 21:19:33 +0100

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -3,21 +3,23 @@ import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_PLATFORM, CONF_WEB_SERVER
 from esphome.components import web_server_base, audio
 
-DEPENDENCIES = ['media_player', 'web_server_base', 'audio']
+DEPENDENCIES = ['media_player', 'web_server_base', 'audio', 'sd_mmc_card'] #Add sd_mmc_card dependency
 CODEOWNERS = ["@votre_nom"]
 
 CONF_STORAGE = "storage"
 CONF_FILES = "files"
 CONF_PATH = "path"
 CONF_MEDIA_FILE = "media_file"
+CONF_CHUNK_SIZE = "chunk_size" #Add chunksize option
 
 storage_ns = cg.esphome_ns.namespace('storage')
 StorageComponent = storage_ns.class_('StorageComponent', cg.Component)
-StorageFile = storage_ns.class_('StorageFile', audio.AudioFile, cg.Component)  # Héritage double
+StorageFile = storage_ns.class_('StorageFile', audio.AudioFile, cg.Component)
 
 FILE_SCHEMA = cv.Schema({
     cv.Required(CONF_PATH): cv.string,
     cv.Required(CONF_ID): cv.declare_id(StorageFile),
+    cv.Optional(CONF_CHUNK_SIZE, default="512"): cv.positive_int, #Default chunk size
 })
 
 CONFIG_SCHEMA = cv.Schema({
@@ -27,22 +29,25 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_WEB_SERVER): cv.use_id(web_server_base.WebServerBase),
 }).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
+@cg.coroutine
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    
+    yield cg.register_component(var, config)
+
     cg.add(var.set_platform(config[CONF_PLATFORM]))
-    
+
     for file in config[CONF_FILES]:
         file_var = cg.new_Pvariable(file[CONF_ID])
         cg.add(file_var.set_path(file[CONF_PATH]))
         cg.add(file_var.set_platform(config[CONF_PLATFORM]))
-        await cg.register_component(file_var, file)  # Enregistre comme Component
+        cg.add(file_var.set_chunk_size(file[CONF_CHUNK_SIZE]))  # Set Chunk Size
+        yield cg.register_component(file_var, file)
         cg.add(var.add_file(file_var))
-    
+
     if CONF_WEB_SERVER in config:
-        web_server = await cg.get_variable(config[CONF_WEB_SERVER])
+        web_server = yield cg.get_variable(config[CONF_WEB_SERVER])
         cg.add(var.set_web_server(web_server))
+
 
 
 
