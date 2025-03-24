60b08bc089f2886d1827ae7569a886540db695cd
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-24 05:08:19 +0100

    Update __init__.py

--- components/box3web/__init__.py
+++ components/box3web/__init__.py
@@ -1,21 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sd_mmc_card
-from esphome.const import CONF_ID, CONF_URL, CONF_PORT
+from esphome.const import CONF_ID, CONF_URL_PREFIX, CONF_ROOT_PATH, CONF_ENABLE_DELETION, CONF_ENABLE_DOWNLOAD, CONF_ENABLE_UPLOAD
 
-# Nouvelles constantes pour ESPAsyncWebServer
+# Import AsyncWebServer
+from esphome.components.async_web_server import AsyncWebServer
+
+# Constants
 CONF_ASYNC_WEB_SERVER_ID = "async_web_server_id"
 
-# Espace de noms pour le composant
+# Namespace
 Box3Web_ns = cg.esphome_ns.namespace("box3web")
 Box3Web = Box3Web_ns.class_("Box3Web", cg.Component)
 
-# Schéma de validation de la configuration
+# Configuration schema
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(Box3Web),
-            cv.GenerateID(CONF_ASYNC_WEB_SERVER_ID): cv.use_id(AsyncWebServer),  # Utiliser AsyncWebServer
+            cv.GenerateID(CONF_ASYNC_WEB_SERVER_ID): cv.use_id(AsyncWebServer),  # Use AsyncWebServer
             cv.GenerateID(sd_mmc_card.CONF_SD_MMC_CARD_ID): cv.use_id(sd_mmc_card.SdMmc),
             cv.Optional(CONF_URL_PREFIX, default="file"): cv.string_strict,
             cv.Optional(CONF_ROOT_PATH, default="/"): cv.string_strict,
@@ -26,30 +29,31 @@ CONFIG_SCHEMA = cv.All(
     ).extend(cv.COMPONENT_SCHEMA),
 )
 
-# Priorité de génération du code
 @coroutine_with_priority(45.0)
 async def to_code(config):
-    # Récupérer l'instance de AsyncWebServer
+    # Get the AsyncWebServer instance
     server_var = await cg.get_variable(config[CONF_ASYNC_WEB_SERVER_ID])
 
-    # Créer une instance de Box3Web
+    # Create Box3Web instance
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Configurer la carte SD
+    # Configure SD card
     sdmmc = await cg.get_variable(config[sd_mmc_card.CONF_SD_MMC_CARD_ID])
     cg.add(var.set_sd_mmc_card(sdmmc))
 
-    # Configurer les options
+    # Set URL prefix and root path
     cg.add(var.set_url_prefix(config[CONF_URL_PREFIX]))
     cg.add(var.set_root_path(config[CONF_ROOT_PATH]))
+
+    # Enable features
     cg.add(var.set_deletion_enabled(config[CONF_ENABLE_DELETION]))
     cg.add(var.set_download_enabled(config[CONF_ENABLE_DOWNLOAD]))
     cg.add(var.set_upload_enabled(config[CONF_ENABLE_UPLOAD]))
 
-    # Ajouter Box3Web comme gestionnaire au serveur AsyncWebServer
+    # Add Box3Web as a handler to the AsyncWebServer
     cg.add(server_var.add_handler(var))
 
-    # Définir une macro pour indiquer que nous utilisons ESPAsyncWebServer
+    # Define macro for ESPAsyncWebServer usage
     cg.add_define("USE_ESPASYNCWEBSERVER")
     
