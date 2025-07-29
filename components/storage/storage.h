cef54e99b1a873cfd6b35a76513bf1b2d785f106
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-29 18:00:32 +0200

    Update storage.h

--- components/storage/storage.h
+++ components/storage/storage.h
@@ -17,9 +17,17 @@ struct FileInfo {
   FileInfo();
 };
 
-class Storage : public EntityBase {
+class Storage : public Component {  // ← CHANGEMENT ICI : EntityBase → Component
  public:
-  // direct functions
+  // Ajouter les méthodes Component obligatoires
+  void setup() override {}
+  void dump_config() override {}
+  
+  // Ajouter les méthodes pour la configuration depuis Python
+  void set_path_prefix(const std::string &prefix) { this->path_prefix_ = prefix; }
+  void set_sd_mmc_card(void* sd_mmc) { this->sd_mmc_card_ = sd_mmc; }
+  
+  // Vos méthodes existantes (inchangées)
   virtual uint8_t direct_read_byte(size_t offset) = 0;
   virtual bool direct_write_byte(uint8_t data) = 0;
   virtual bool direct_append_byte(uint8_t data) = 0;
@@ -36,29 +44,19 @@ class Storage : public EntityBase {
   bool write_array(uint8_t *data, size_t data_length);
   bool append_array(uint8_t *data, size_t data_length);
 
-  // void write_buffer();
-  // void refresh_buffer(uint32_t offset = 0);
-  // uint8_t & operator[] (size_t index);
-  // uint32_t get_buffer_size();
-  // void write_on_shutdown(bool value);
-
  protected:
   virtual void direct_set_file(const std::string &file) = 0;
   virtual FileInfo direct_get_file_info(const std::string &path) = 0;
   virtual std::vector<FileInfo> direct_list_directory(const std::string &path) = 0;
-  // void load_buffer (uint32_t offset, uint32_t buffer_offset, uint32_t length);
-  // void write_buffer (uint32_t offset, uint32_t buffer_offset, uint32_t length);
-  // void allocate_buffer(uint32_t buffer_size);
   void update_offset(size_t value);
-  // uint8_t * buffer_;
-  // uint32_t buffer_size_;
-  // uint32_t buffer_offset_;
   FileInfo *current_file_;
-  // uint32_t base_offset_;
-  // uint32_t max_offset_;
-  // bool write_on_shutdown_;
+  
+  // Ajouter les membres pour la configuration
+  std::string path_prefix_;
+  void* sd_mmc_card_{nullptr};
 };
 
+// StorageClient reste inchangé
 class StorageClient : public EntityBase {
  public:
   std::vector<FileInfo> list_directory(const std::string &path);
@@ -82,6 +80,7 @@ class StorageClient : public EntityBase {
 
 }  // namespace storage
 }  // namespace esphome
+
 
 
 
