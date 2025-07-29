141feeca381e5c3cd9c32b84a46052293014993e
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-29 17:36:58 +0200

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -1,51 +1,39 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_PLATFORM, CONF_WEB_SERVER
-from esphome.components import web_server_base, audio
+from esphome.const import CONF_ID
+from esphome.cpp_generator import MockObjClass
 
-DEPENDENCIES = ['media_player', 'web_server_base', 'audio', 'sd_mmc_card'] #Add sd_mmc_card dependency
-CODEOWNERS = ["@votre_nom"]
+storage_ns = cg.esphome_ns.namespace("storage")
+Storage = storage_ns.class_("Storage", cg.EntityBase)
+StorageClient = storage_ns.class_("StorageClient", cg.EntityBase)
+StorageClientStatic = storage_ns.namespace("StorageClient")
 
-CONF_STORAGE = "storage"
-CONF_FILES = "files"
-CONF_PATH = "path"
-CONF_MEDIA_FILE = "media_file"
-CONF_CHUNK_SIZE = "chunk_size" #Add chunksize option
+IS_PLATFORM_COMPONENT = True
 
-storage_ns = cg.esphome_ns.namespace('storage')
-StorageComponent = storage_ns.class_('StorageComponent', cg.Component)
-StorageFile = storage_ns.class_('StorageFile', audio.AudioFile, cg.Component)
+CONF_PREFIX = "path_prefix"
 
-FILE_SCHEMA = cv.Schema({
-    cv.Required(CONF_PATH): cv.string,
-    cv.Required(CONF_ID): cv.declare_id(StorageFile),
-    cv.Optional(CONF_CHUNK_SIZE, default="512"): cv.positive_int, #Default chunk size
-})
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.Required(CONF_PLATFORM): cv.one_of("sd_card", "flash", "inline", lower=True),
-    cv.Required(CONF_ID): cv.declare_id(StorageComponent),
-    cv.Required(CONF_FILES): cv.ensure_list(FILE_SCHEMA),
-    cv.Optional(CONF_WEB_SERVER): cv.use_id(web_server_base.WebServerBase),
-}).extend(cv.COMPONENT_SCHEMA)
+STORAGE_SCHEMA = cv.Schema(
+    {cv.GenerateID(): cv.declare_id(Storage), cv.Required(CONF_PREFIX): cv.string}
+)
 
-async def to_code(config): #Replaced @cg.coroutine with async def
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config) #Replaced yield with await
 
-    cg.add(var.set_platform(config[CONF_PLATFORM]))
+def storage_schema(
+    class_: MockObjClass = cv.UNDEFINED,
+) -> cv.Schema:
+    schema = {}
 
-    for file in config[CONF_FILES]:
-        file_var = cg.new_Pvariable(file[CONF_ID])
-        cg.add(file_var.set_path(file[CONF_PATH]))
-        cg.add(file_var.set_platform(config[CONF_PLATFORM]))
-        cg.add(file_var.set_chunk_size(file[CONF_CHUNK_SIZE]))  # Set Chunk Size
-        await cg.register_component(file_var, file) #Replaced yield with await
-        cg.add(var.add_file(file_var))
+    if class_ is not cv.UNDEFINED:
+        # Not cv.optional
+        schema[cv.GenerateID()] = cv.declare_id(class_)
 
-    if CONF_WEB_SERVER in config:
-        web_server = await cg.get_variable(config[CONF_WEB_SERVER]) #Replaced yield with await
-        cg.add(var.set_web_server(web_server))
+    return STORAGE_SCHEMA.extend(schema)
+
+
+async def storage_to_code(config):
+    storage = await cg.get_variable(config[CONF_ID])
+    prefix = config[CONF_PREFIX]
+    cg.add(StorageClientStatic.add_storage(storage, prefix))
 
 
 
